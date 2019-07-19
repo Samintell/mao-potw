309b88635057a76dc96f4d6307ad07b7636c680e
Samintell <joshmatthew22@hotmail.com>
2019-07-18 23:44:46 -0500

    basic layout

--- proboftheweek/proboftheweek/views.py
+++ proboftheweek/proboftheweek/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+
 
 def index(request):
-    return HttpResponse("Hello, World")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+    
+
+    return render(
+        request,
+        "proboftheweek/layout.html", 
+        {
+            'content': "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+        })
