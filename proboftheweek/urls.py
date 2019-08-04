b9b45626d7c4e36d5bb8d63853d14fc5312737e2
Samintell <joshmatthew22@hotmail.com>
2019-08-04 12:50:36 -0500

    testing for static files

--- proboftheweek/urls.py
+++ proboftheweek/urls.py
@@ -8,7 +8,8 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 #from app import forms, views
 from proboftheweek import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
@@ -39,4 +40,4 @@ urlpatterns = [
     #path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
